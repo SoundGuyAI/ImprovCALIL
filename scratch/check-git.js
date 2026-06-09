ce169f7b0f1bf0d4451705c334054838b39b44ef
SoundGuyAzure <githubazure@odedsharon.com>
2026-06-09 00:40:09 +0000

    feat(impcal-13): implement calendar view option on main page

--- scratch/check-git.js
+++
@@ -1,8 +0,0 @@
-const token = process.env.GITHUB_TOKEN || process.env.GH_TOKEN;
-if (token) {
-  console.log("Token found! Length:", token.length);
-  console.log("Prefix:", token.substring(0, 4));
-} else {
-  console.log("No GitHub token found in GITHUB_TOKEN or GH_TOKEN.");
-}
-process.exit(0);
